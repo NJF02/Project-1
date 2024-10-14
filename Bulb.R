eec8cf98cdb0835a8730f664e798addd3c5e6f57
Jun Fei <ngjunfei2002@gmail.com>
2024-10-14 23:41:46 +0800

    Updated more code

--- Bulb.R
+++ Bulb.R
@@ -1,5 +1,7 @@
 library(dplyr)
 library(ggplot2)
+library(car)
+library(lme4)
 
 # Change to your own directory
 
@@ -12,6 +14,15 @@ data$Bulb = factor(data$Bulb)
 data$Machine = factor(data$Machine)
 data$Station = factor(data$Station)
 
+# Calculate the average of Watts for each Bulb
+
+WattsbyMachine <- data %>%
+  group_by(Bulb, Machine) %>%
+  summarise(Watts = mean(Watts))
+
+WattsbyStation <- data %>%
+  group_by(Machine, Station) %>%
+  summarise(Watts = mean(Watts))
 
 # Exploratory Analysis
 
@@ -19,8 +30,6 @@ Station1 = filter(data, Station == '1')
 Station2 = filter(data, Station == '2')
 Station3 = filter(data, Station == '3')
 Station4 = filter(data, Station == '4')
-
-
 
 # Histograms
 
@@ -42,6 +51,13 @@ hist2
 hist3
 hist4
 
+WattsbyMachine %>% ggplot(aes(x = Machine, y = Watts)) + geom_boxplot()
+data %>% ggplot(aes(x = Machine, y = Watts)) + geom_boxplot()
+
+data %>% ggplot(aes(x = Station, y = Watts)) + geom_boxplot()
+WattsbyStation %>% ggplot(aes(x = Station, y = Watts)) + geom_boxplot()
+anova(WattsbyStation)
+
 # Box Plots
 
 box0 = ggplot(data, aes(Station, Watts, colour = Station)) + geom_boxplot()
@@ -57,6 +73,13 @@ box2
 box3
 box4
 
+# Pre Analysis
+
+shapiro.test(Station1$Watts)
+shapiro.test(Station2$Watts)
+shapiro.test(Station3$Watts)
+shapiro.test(Station4$Watts)
+
 # Model Fitting
 
 fit1 = lm(Watts ~ Machine, data = Station1)
@@ -71,4 +94,43 @@ summary(fit4)
 
 fit5 = lm(Watts ~ Station, data = data)
 summary(fit5)
+
+aov.Station = aov(Watts ~ Station, data = data)
+summary(aov.Station)
+TukeyHSD(aov.Station)
+plot(TukeyHSD(aov.Station))
+
+
+WattsbyMachine$Machine = relevel(WattsbyMachine$Machine, ref = "D")
+
+
+fit6 = lm(Watts ~ Machine, data = WattsbyMachine)
+fit7 = lm(Watts ~ Station, data = WattsbyStation)
+summary(fit6)
+summary(fit7)
+
+
+# Mixed Effects Model
+Watts.null.Machine = lmer(Watts ~ (1 | Station), data = data)
+Watts.Machine = lmer(Watts ~ Machine + (1 | Station), data = data)
+Watts.null.Station = lmer(Watts ~ (1 | Machine), data = data)
+Watts.Station = lmer(Watts ~ Station + (1 | Machine), data = data)
+
+coef(Watts.Machine)
+
+anova(Watts.nullM, Watts.Machine)
+anova(Watts.nullS, Watts.Station)
+
+
+data$Machine = relevel(data$Machine, ref = "D")
+
+Watts.nullMPlus = lmer(Watts ~ (1 + Machine | Station), data = data)
+Watts.MachinePlus = lmer(Watts ~ Machine + (1 + Machine | Station), data = data)
+
+coef(Watts.MachinePlus)
+anova(Watts.nullMPlus, Watts.MachinePlus)
+
+# Model Diagnostic
+
+influencePlot(fit5)
 
